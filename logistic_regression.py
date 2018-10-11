dfafdda9ca022d2d6d14e8b6c5870bde91bda671
zhaoyi3 <zhaoyi3@lenovo.com>
2018-10-11 14:20:50 +0800

    add logistic regression algo

---
+++ logistic_regression.py
@@ -0,0 +1,33 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import metrics
+
+class logistic_regression:
+    def __sigmoid(self, x):
+        return 1 / (1 + np.exp(-x))
+
+    def fit(self, X, y, learning_rate, epochs):
+        data_number, feature_number = X.shape
+
+        self.__W = np.zeros((feature_number, 1))
+        self.__b = 0
+
+        accuracy = []
+        loss = []
+        for _ in range(epochs):
+            h = self.__sigmoid(X.dot(self.__W) + self.__b)
+            self.__W -= learning_rate * X.T.dot(h - y) / data_number
+            self.__b -= learning_rate * np.mean(h - y)
+
+            h = self.__sigmoid(X.dot(self.__W) + self.__b)
+            loss.append(np.mean((h - y) ** 2))
+            accuracy.append(metrics.accuracy(y, h > 0.5))
+
+        _, ax_loss = plt.subplots()
+        ax_loss.plot(loss, 'b')
+        ax_accuracy = ax_loss.twinx()
+        ax_accuracy.plot(accuracy, 'r')
+        plt.show()
+
+    def predict(self, X):
+        return self.__sigmoid(X.dot(self.__W) + self.__b) > 0.5
