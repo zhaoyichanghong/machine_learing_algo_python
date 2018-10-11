5a3313808148d83f844e9abcfcb1ead874f935c3
zhaoyi3 <zhaoyi3@lenovo.com>
2018-10-11 10:48:23 +0800

    modify r2 score

--- metrics.py
+++ metrics.py
@@ -2,3 +2,6 @@ import numpy as np
 
 def accuracy(y_true, y_pred):
     return np.mean(y_true == y_pred)
+
+def r2_score(y_true, y_pred):
+    return 1 - np.sum((y_true - y_pred) ** 2) / np.sum((y_true - np.mean(y_true)) ** 2)
