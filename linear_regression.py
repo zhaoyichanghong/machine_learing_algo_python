2ca50a9ac259e7447166fc83f5e6f5c29b759859
zhaoyi3 <zhaoyi3@lenovo.com>
2018-10-11 13:27:04 +0800

    add linear regression newton algo

--- linear_regression.py
+++ linear_regression.py
@@ -23,6 +23,34 @@ class linear_regression_gradient_descent:
     def predict(self, X):
         return X.dot(self.__W) + self.__b
 
+class linear_regression_newton:
+    def fit(self, X, y, epochs):
+        data_number, feature_number = X.shape
+
+        self.__W = np.zeros((feature_number, 1))
+        self.__b = 0
+
+        loss = []
+        for _ in range(epochs):
+            h = X.dot(self.__W) + self.__b
+
+            w_g = X.T.dot(h - y)
+            w_H = X.T.dot(X)
+            self.__W -= np.linalg.pinv(w_H).dot(w_g)
+
+            b_g = np.sum(h - y)
+            b_H = data_number
+            self.__b -= b_g / b_H
+
+            h = X.dot(self.__W) + self.__b
+            loss.append(np.mean((h - y) ** 2))
+
+        plt.plot(loss)
+        plt.show()
+
+    def predict(self, X):
+        return X.dot(self.__W) + self.__b
+
 class linear_regression_equation:
     def fit(self, X, y):
         X = np.insert(X, 0, 1, axis=1)
