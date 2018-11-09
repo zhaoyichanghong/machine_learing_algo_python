ca66c9b123af32d1d8785e4b740953e0ab16d31e
zhaoyi <zhaoyi3@lenovo.com>
2018-11-09 15:13:24 +0800

    optimize knn

--- knn.py
+++ knn.py
@@ -12,8 +12,7 @@ class knn:
         y_pred = np.zeros((data_number, 1))
         for i in range(data_number):
             distance = np.linalg.norm(X[i] - self.__X, axis=1)
-            sort = np.argsort(distance)
-
-            y_pred[i] = np.argmax(np.bincount(self.__y[sort[:self.__k]].flatten().astype(int)))
+            nearest_items = np.argpartition(distance, self.__k - 1)[:self.__k][0]
+            y_pred[i] = np.argmax(np.bincount(self.__y[nearest_items].astype(int)))
         
         return y_pred
